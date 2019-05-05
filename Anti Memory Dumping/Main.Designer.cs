5e5f60ec43b24107426c13837cbc8eab86f95469
Daniel Jacob <45327377+iYaReM-RE@users.noreply.github.com>
2019-05-05 05:56:42 -0300

    Add files via upload

---
+++ Anti Memory Dumping/Main.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Anti_Memory_Dumping
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "I Love You!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 70);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Erase PE (Dump)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Anti Memory Dumping/Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Anti_Memory_Dumping
+{
+    public partial class Main : Form
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr ZeroMemory(IntPtr addr, IntPtr size);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr VirtualProtect(IntPtr lpAddress, IntPtr dwSize, IntPtr flNewProtect, ref IntPtr lpflOldProtect);
+
+        public static void EraseSection(IntPtr address, int size)
+        {
+            IntPtr sz = (IntPtr)size;
+            IntPtr dwOld = default(IntPtr);
+            VirtualProtect(address, sz, (IntPtr)0x40, ref dwOld);
+            ZeroMemory(address, sz);
+            IntPtr temp = default(IntPtr);
+            VirtualProtect(address, sz, dwOld, ref temp);
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+
+            int[] sectiontabledwords = new int[] { 0x8, 0xC, 0x10, 0x14, 0x18, 0x1C, 0x24 };
+            int[] peheaderbytes = new int[] { 0x1A, 0x1B };
+            int[] peheaderwords = new int[] { 0x4, 0x16, 0x18, 0x40, 0x42, 0x44, 0x46, 0x48, 0x4A, 0x4C, 0x5C, 0x5E };
+
+            var process = System.Diagnostics.Process.GetCurrentProcess();
+            var base_address = process.MainModule.BaseAddress;
+            var dwpeheader = Marshal.ReadInt32((IntPtr)(base_address.ToInt32() + 0x3C));
+            var wnumberofsections = Marshal.ReadInt16((IntPtr)(base_address.ToInt32() + dwpeheader + 0x6));
+
+            for (int i = 0; i < peheaderwords.Length; i++)
+            {
+                EraseSection((IntPtr)(base_address.ToInt32() + dwpeheader + peheaderwords[i]), 1);
+            }
+            for (int i = 0; i < peheaderbytes.Length; i++)
+            {
+                EraseSection((IntPtr)(base_address.ToInt32() + dwpeheader + peheaderbytes[i]), 2);
+            }
+
+            int x = 0;
+            int y = 0;
+
+            while (x <= wnumberofsections)
+            {
+                if (y == 0)
+                {
+                    EraseSection((IntPtr)((base_address.ToInt32() + dwpeheader + 0xFA + (0x28 * x)) + 0x20), 2);
+                }
+
+                y++;
+
+                if (y == sectiontabledwords.Length)
+                {
+                    x++;
+                    y = 0;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I don't know why I made this button haha", "iYaReM", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
